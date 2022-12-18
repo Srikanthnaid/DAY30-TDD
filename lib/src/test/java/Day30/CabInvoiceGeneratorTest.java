b71fa36e3d8284064c51ccc1e2c89425042bfaac
Srikanthnaid <ksru044gmail.com>
2022-12-18 18:39:38 +0530

    Calculate multiple rides of total fares

--- lib/src/test/java/Day30/CabInvoiceGeneratorTest.java
+++ lib/src/test/java/Day30/CabInvoiceGeneratorTest.java
@@ -5,10 +5,6 @@ import org.junit.jupiter.api.Test;
 
 class CabInvoiceGeneratorTest {
 
-	/**
-	 * Test method to calculate total fare of the ride by given distance and time as
-	 * the input parameters
-	 */
 	@Test
 	public void whenGivenDistanceAndTimeShouldReturnTotalFare() {
 		double distance = 2.0;
@@ -17,5 +13,29 @@ class CabInvoiceGeneratorTest {
 		double totalFare = CIG.calculateFare(distance, time);
 		System.out.println("Total Fare is:" + totalFare);
 		Assertions.assertEquals(25, totalFare, 0.0);
+	}
+
+	/**
+	 * Method to calculate minimum fare for the given ride for that minimum fare we
+	 * have taken distance as 0.1 and time is 2.
+	 */
+	@Test
+	public void whenGivenLessDistanceOrTimeShouldReturnMinimumFare() {
+		double distance = 0.1;
+		int time = 1;
+		CabInvoiceGenerator CIG = new CabInvoiceGenerator();
+
+		double fare = CIG.calculateFare(distance, time);
+		System.out.println("Minimum Fare = " + fare);
+		Assertions.assertEquals(5, fare, 0.0);
+	}
+
+	@Test
+	public void whenGivenMultipleRidesShouldReturnTotalFare() {
+		Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+		CabInvoiceGenerator CIG = new CabInvoiceGenerator();
+		double totalFare = CIG.calculateTotalFare(rides);
+		System.out.println("Total Fare = " + totalFare);
+		Assertions.assertEquals(30, totalFare, 0.0);
 	}
 }
