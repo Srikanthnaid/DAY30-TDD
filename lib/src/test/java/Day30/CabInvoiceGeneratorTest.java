ad8537c140647bf7cf5b2fa7c4cf38b5a68cb7b6
Srikanthnaid <ksru044gmail.com>
2022-12-18 18:46:31 +0530

    7KH ,QYRLFH *HQHUDWRU VKRXOG QRZ UHWXUQ WKH

--- lib/src/test/java/Day30/CabInvoiceGeneratorTest.java
+++ lib/src/test/java/Day30/CabInvoiceGeneratorTest.java
@@ -38,4 +38,25 @@ class CabInvoiceGeneratorTest {
 		System.out.println("Total Fare = " + totalFare);
 		Assertions.assertEquals(30, totalFare, 0.0);
 	}
+	@Test
+    public void givenMultipleRides_shouldReturnSizeAndAverageFare(){
+        CabInvoiceGenerator CIG = new CabInvoiceGenerator();
+
+        Ride[] rides = { new Ride(20.0,4),
+                         new Ride(45.0,1),
+                         new Ride(75.0,1),
+                         new Ride(45.5,1)};
+
+        double totalFare = CIG.calculateTotalFare(rides);
+        int numberOfRides = CIG.getNumberOfRides(rides);
+        double averageTotalFare = CIG.calculateAverageRideCost(rides);
+
+        System.out.println("Total Fare = " +totalFare);
+        System.out.println("Number of ride = " +numberOfRides);
+        System.out.println("Average Total Fare " +averageTotalFare);
+
+        Assertions.assertEquals(1862,totalFare,0.0);
+        Assertions.assertEquals(4,numberOfRides);
+        Assertions.assertEquals(465,averageTotalFare,0.5);
+	}
 }
